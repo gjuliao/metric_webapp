2a1dfb5a16d5b6c76ddb4071034bf5e3d9f42399
gjuliao <gjuliao32@hotmail.com>
2023-02-06 08:37:57 -0500

    Added routing dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div className="App">
       <h2>hello world</h2>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
