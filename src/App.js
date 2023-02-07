5acdd196b582de7d77d3a4f5bebf0fe9cb7b4895
gjuliao <gjuliao32@hotmail.com>
2023-02-07 11:19:00 -0500

    Changed routing structure to fit design

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import Nav from './components/navbar/Nav';
 import Country from './components/country/Country';
+import Europe from './components/country/europe/Europe';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Europe />} />
+        <Route path="/europe" element={<Home />} />
         <Route path="/:country" element={<Country />} />
       </Routes>
     </div>
