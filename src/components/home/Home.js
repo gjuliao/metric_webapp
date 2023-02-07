3993bb820b383ed968253fca4f8ba7087ebec508
gjuliao <gjuliao32@hotmail.com>
2023-02-07 09:47:51 -0500

    Added filter input and working

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { useSelector } from 'react-redux';
 import CountryCard from '../countrycard/CountryCard';
 
 const Home = () => {
+  const [query, setQuery] = useState({
+    query: '',
+    list: [],
+  });
+
   const countries = useSelector((state) => state.data);
+
+  const handleChange = (e) => {
+    const results = countries.filter((el) => {
+      if (e.target.value === '') return countries;
+      return el.name.common.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    setQuery({
+      query: e.target.value,
+      list: results,
+    });
+  };
+  // eslint-disable-next-line no-console
+  console.log(query.list, 'from query');
   return (
     <div className="home">
-      { countries.map((country) => (
+      <form>
+        <input type="search" value={query.query} onChange={handleChange} />
+      </form>
+      { query.list.map((country) => (
         <div key={country.name.common} className="country">
           <CountryCard
             population={country.population}
